62d186e58046e93950770514f6c07e8e76e4dbf6
kikeiz <kikeizp96@gmail.com>
2023-04-12 16:31:55 +0200

    allow headeer

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(fileUpload())
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://react-website-bucket-kike.s3-website.eu-north-1.amazonaws.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
